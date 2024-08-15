aba2ca4be9468e07b42dadcc2d68625569bd0408
Billy19191 <bill.ratchanon@gmail.com>
2024-08-16 03:46:24 +0700

    feat: split urls to chuck (avoid rate-limit)

--- index.ts
+++ index.ts
@@ -1,30 +1,44 @@
-import { SITEMAP_FORUM, baseUrl } from "./const";
-import { GetLinkCategory } from "./script/forum/category/get-categories";
-import { SaveProjectData } from "./script/forum/project/get-project-detail";
-import { ListAllProjectInCategory } from "./script/forum/project/get-projects";
-import { GetAllProjectsUrl } from "./script/forum/sitemap/sitemap";
-import { htmlToText } from 'html-to-text';
+import { SITEMAP_FORUM, baseUrl } from './const'
+import { GetLinkCategory } from './script/forum/category/get-categories'
+import { SaveProjectData } from './script/forum/project/get-project-detail'
+import { ListAllProjectInCategory } from './script/forum/project/get-projects'
+import { GetAllProjectsUrl } from './script/forum/sitemap/sitemap'
+import { htmlToText } from 'html-to-text'
 
 async function main() {
-    // * If sitemap doesn't gave all the result can scrape directly from the website
-    // console.log("on-process: 'getting all categories' . . .")
+  // * If sitemap doesn't gave all the result can scrape directly from the website
+  // console.log("on-process: 'getting all categories' . . .")
 
-    // const categoryHrefs = await GetLinkCategory(baseUrl);
-    // if (categoryHrefs.length === 0){
-    //     console.error("Category Href link is empty array")
-    //     return
-    // }
+  // const categoryHrefs = await GetLinkCategory(baseUrl);
+  // if (categoryHrefs.length === 0){
+  //     console.error("Category Href link is empty array")
+  //     return
+  // }
 
-    // console.log("done: 'getting all categories' working-on: 'list all projects in each category' . . .")
+  // console.log("done: 'getting all categories' working-on: 'list all projects in each category' . . .")
 
-    // const projects = await ListAllProjectInCategory(categoryHrefs)
+  // const projects = await ListAllProjectInCategory(categoryHrefs)
 
-    // console.log("done: 'list all projects in each category' working-on: 'get each project detail' . . .")
+  // console.log("done: 'list all projects in each category' working-on: 'get each project detail' . . .")
 
-    const urls = await GetAllProjectsUrl(SITEMAP_FORUM)
-    
-    await SaveProjectData(urls)
+  function chunkArray<T>(array: T[], size: number): T[][] {
+    const chunkedArray: T[][] = []
+    for (let i = 0; i < array.length; i += size) {
+      chunkedArray.push(array.slice(i, i + size))
+    }
+    return chunkedArray
+  }
 
+  const urls = await GetAllProjectsUrl(SITEMAP_FORUM)
+  const chunkSize = 100
+  const chunkedUrls = chunkArray(urls, chunkSize)
+
+  console.log(urls.length)
+  for (const chunk of chunkedUrls) {
+    await SaveProjectData(chunk)
+    setTimeout(() => {}, 1000)
+  }
+  //   await SaveProjectData(urls)
 }
 
-main();
+main()
--- script/forum/sitemap/sitemap.ts
+++ script/forum/sitemap/sitemap.ts
@@ -1,28 +1,32 @@
-import axios from 'axios';
-import { parseStringPromise } from 'xml2js';
+import axios from 'axios'
+import { parseStringPromise } from 'xml2js'
 
 export async function GetAllProjectsUrl(url: string): Promise<string[]> {
-    try {
-        const response = await axios.get(url);
-        const xmlData = response.data;
+  try {
+    const response = await axios.get(url)
+    const xmlData = response.data
 
-        const parsedData = await parseStringPromise(xmlData, { explicitArray: false });
+    const parsedData = await parseStringPromise(xmlData, {
+      explicitArray: false,
+    })
 
-        const urlset = parsedData['urlset'];
+    const urlset = parsedData['urlset']
 
-        const urls: string[] = urlset['url'].map((entry: any) => {
-            const originalUrl = entry['loc'];
-            const idMatch = originalUrl.match(/\/t\/[^/]+\/(\d+)/);
-            if (idMatch && idMatch[1]) {
-                const id = idMatch[1];
-                return `https://gov.optimism.io/t/${id}.json?forceLoad=true`;
-            }
-            return null;
-        }).filter((url:string | null) => url !== null); // Filter out any null values
+    const urls: string[] = urlset['url']
+      .map((entry: any) => {
+        const originalUrl = entry['loc']
+        const idMatch = originalUrl.match(/\/t\/[^/]+\/(\d+)/)
+        if (idMatch && idMatch[1]) {
+          const id = idMatch[1]
+          return `https://gov.optimism.io/t/${id}.json?forceLoad=true`
+        }
+        return null
+      })
+      .filter((url: string | null) => url !== null) // Filter out any null values
 
-        return urls;
-    } catch (error) {
-        console.error('Error fetching or parsing the XML:', error);
-        return [];
-    }
+    return urls
+  } catch (error) {
+    console.error('Error fetching or parsing the XML:', error)
+    return []
+  }
 }
